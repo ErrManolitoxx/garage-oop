d0db8515a3dc82d48872ff9c2944fffd42e27521
Allan <allanglopez1312@gmail.com>
2024-11-15 14:26:52 +0100

    Hemos implementado el vehículo con todas sus clases y demás

--- src/org/ies/vehicles/components/VehicleReader.java
+++ src/org/ies/vehicles/components/VehicleReader.java
@@ -1,4 +1,24 @@
 package org.ies.vehicles.components;
 
+import org.ies.vehicles.model.Vehicle;
+
+import java.util.Scanner;
+
 public class VehicleReader {
+    private final Scanner scanner;
+
+    public VehicleReader(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public Vehicle askVehicle() {
+        System.out.println("Introduzca los datos del coche: ");
+
+        System.out.println("De qué tipo es su vehículo: ");
+        String type = scanner.nextLine();
+
+        System.out.println("¿Cuál es la velocidad máxima que alcanza su vehículo?");
+        int maxVelocdad = scanner.nextInt();
+    }
+
 }
--- src/org/ies/vehicles/model/Vehicle.java
+++ src/org/ies/vehicles/model/Vehicle.java
@@ -1,7 +1,73 @@
 package org.ies.vehicles.model;
 
+import java.util.Objects;
+
 public class Vehicle {
-    private int velocidad;
+
+    private VehicleType vehicleType;
+    private int maxVelocidad;
     private String color;
     private String matrícula;
+
+    public Vehicle(VehicleType vehicleType, int maxVelocidad, String color, String matrícula) {
+        this.vehicleType = vehicleType;
+        this.maxVelocidad = maxVelocidad;
+        this.color = color;
+        this.matrícula = matrícula;
+    }
+
+    public VehicleType getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(VehicleType vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+    public int getMaxVelocidad() {
+        return maxVelocidad;
+    }
+
+    public void setMaxVelocidad(int maxVelocidad) {
+        this.maxVelocidad = maxVelocidad;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getMatrícula() {
+        return matrícula;
+    }
+
+    public void setMatrícula(String matrícula) {
+        this.matrícula = matrícula;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vehicle vehicle = (Vehicle) o;
+        return maxVelocidad == vehicle.maxVelocidad && vehicleType == vehicle.vehicleType && Objects.equals(color, vehicle.color) && Objects.equals(matrícula, vehicle.matrícula);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(vehicleType, maxVelocidad, color, matrícula);
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "vehicleType=" + vehicleType +
+                ", maxVelocidad=" + maxVelocidad +
+                ", color='" + color + '\'' +
+                ", matrícula='" + matrícula + '\'' +
+                '}';
+    }
 }
