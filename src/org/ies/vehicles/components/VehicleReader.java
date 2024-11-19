4b5d91bbe922ac78df17c344a10b5314ec3c1bb5
Allan <allanglopez1312@gmail.com>
2024-11-19 12:39:21 +0100

    He metido en vehiclereader los scanner para el color y la matrícula.

--- src/org/ies/vehicles/components/VehicleReader.java
+++ src/org/ies/vehicles/components/VehicleReader.java
@@ -19,6 +19,12 @@ public class VehicleReader {
 
         System.out.println("¿Cuál es la velocidad máxima que alcanza su vehículo?");
         int maxVelocdad = scanner.nextInt();
+
+        System.out.println("De qué color es su coche?");
+        String color = scanner.nextLine();
+
+        System.out.println("Introduzca la matrícula:");
+        String matricula = scanner.nextLine();
     }
 
 }
